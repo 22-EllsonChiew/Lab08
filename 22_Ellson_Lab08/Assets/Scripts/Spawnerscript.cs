677ed1020cee15270010d4e58eecff3a584ae768
Itzuzume <ellson637@gmail.com>
2022-02-07 15:42:18 +0800

    Update 3 random spawn
    modify the spawner script to randomly spawn 3 objects

--- 22_Ellson_Lab08/Assets/Scripts/Spawnerscript.cs
+++ 22_Ellson_Lab08/Assets/Scripts/Spawnerscript.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Spawnerscript : MonoBehaviour
 {
-    public GameObject SpawnObject;
+    //public GameObject SpawnObject;
+
     float PositionY;
+
+    [SerializeField] GameObject[] RandObjects;
+
+    [SerializeField] float seconSpawn = 0.5f;
+
+    [SerializeField] float minTrans;
+
+    [SerializeField] float maxTrans;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnObjects", 1, 1);
+        //InvokeRepeating("SpawnObjects", 1, 1);
+
+        StartCoroutine(ObjectSpawner());
+
     }
 
     // Update is called once per frame
@@ -21,10 +33,26 @@ public class Spawnerscript : MonoBehaviour
 
     }
 
-    void SpawnObjects()
+    IEnumerator ObjectSpawner()
+    {
+        while(true)
+        {
+            var wanted = Random.Range(minTrans, maxTrans);
+            var position = new Vector3(wanted, transform.position.y);
+            this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+            PositionY = Random.Range(4, -4f);
+
+            GameObject gameObject = Instantiate(RandObjects[Random.Range(0, RandObjects.Length)], position, Quaternion.identity);
+            yield return new WaitForSeconds(seconSpawn);
+            Destroy(gameObject, 4f);
+        }
+    }
+
+    /*void SpawnObjects()
     {
         PositionY = Random.Range(4, -4f);
         this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
         Instantiate(SpawnObject, transform.position, transform.rotation);
-    }
+    }*/
+
 }
